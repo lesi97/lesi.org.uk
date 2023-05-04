8a7e5c3d84bffc4c7bab9b6ee160e8d12b182c46
chris-lesi <lesi97@hotmail.co.uk>
2023-05-04 16:27:52 +0100

    remove twitch api

--- JS/scripts.js
+++ JS/scripts.js
@@ -125,31 +125,6 @@ function updateFooter() {
 			}
 		});
 }
-
-/* Requires OAuth with Twitch and I cba doing that - espicially since I don't stream anymore
-function twitchCheckIfLive() {
-	xhr.open('GET', 'https://api.twitch.tv/helix/streams?user_id=101129910', true);
-	xhr.setRequestHeader('client-id', '3owpqp1m6zjzop8q6nsnr2bc171iey');
-	xhr.setRequestHeader('Authorization', 'Bearer cptrvxsphcctou8ngml6b5v4ct8b9b');
-	xhr.send();
-
-	xhr.onload = function () {
-		if (xhr.status === 200) {
-			let response = JSON.parse(xhr.response);
-			console.log(response);
-			let checkLive = response.data[0].type;
-			
-			if (checkLive === 'live') {
-				document.getElementById("twitchIcon").src = "images/socials/twitchLive.png";
-				document.getElementById("twitchIcon").style.width = "30px";
-				document.getElementById("twitchIcon").style.height = "46px";
-			}
-		}
-	};
-}
-*/
-
-
 	
 
 function checkUserPreferences() {
